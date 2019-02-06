613604d34b090d92362393d0183352197654ace1
Milice <robin_norling@outlook.com>
2019-02-06 09:20:18 +0100

    Update main.cpp
    Remove most Windows defined types, there are no reason to use their stupid typedefs.
    Also added the CloseHandle if it opens the process but it failed to inject and added and if statement around CreateRemoteThread to properly check whether it actually did inject.

--- src/injector/main.cpp
+++ src/injector/main.cpp
@@ -7,19 +7,21 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-	unsigned long processId = std::stoul(argv[1]);
 	std::string dllPath(argv[2]);
 
-	if(HANDLE client = OpenProcess(PROCESS_ALL_ACCESS, false, processId)) {
-		if(LPVOID LoadLibA = (LPVOID)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA")) {
-			if(LPVOID allocString = (LPVOID)VirtualAllocEx(client, NULL, dllPath.length(), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)) {
-				if(WriteProcessMemory(client, (LPVOID)allocString, dllPath.c_str(), dllPath.length(), NULL)) {
-					CreateRemoteThread(client, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibA, (LPVOID)allocString, 0, NULL);
-					CloseHandle(client);
-					return 0;
+	if(void* client = OpenProcess(PROCESS_ALL_ACCESS, false, std::stoul(argv[1]))) {
+		if(void* LoadLibA = (void*)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryA")) {
+			if(void* allocString = (void*)VirtualAllocEx(client, NULL, dllPath.length(), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)) {
+				if(WriteProcessMemory(client, (void*)allocString, dllPath.c_str(), dllPath.length(), NULL)) {
+					if(CreateRemoteThread(client, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibA, (void*)allocString, 0, NULL)) {
+						CloseHandle(client);
+						return 0;
+					}
 				}
 			}
 		}
+		CloseHandle(client);
 	}
+
 	return 1;
 }
